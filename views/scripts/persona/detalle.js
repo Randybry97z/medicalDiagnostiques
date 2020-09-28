8b24cfeaa9732af2b9ddda3f3dc72e83252e4d0f
Gabriel Paredes <edwgabo@gmail.com>
2020-09-28 13:58:33 -0500

    Cambiar url de base_url Usuario.js

--- views/scripts/persona/detalle.js
+++ views/scripts/persona/detalle.js
@@ -1,4 +1,4 @@
-const base_url = "http://3.134.247.207/mdt_final/"
+const base_url = "http://localhost/mdt_final/"
 var nombre = document.getElementById('nombre')
 var estatus = document.getElementById('estatus')
 var contacto = document.getElementById('contacto')
--- views/scripts/usuario/updatePassword.js
+++ views/scripts/usuario/updatePassword.js
@@ -1,4 +1,4 @@
-const base_url = "http://3.134.247.207/mdt_final/";
+const base_url = "http://localhost/mdt_final/";
 var btnCancelar = document.getElementById("btnCancelar")
 
 function cancelar() {
